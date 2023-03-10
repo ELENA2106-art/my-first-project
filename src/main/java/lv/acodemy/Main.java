e9df78815a8cc2568b704bde527eb6cff593c21f
ELENA2106-art <Agasem2106@icloud.com>
2023-03-10 12:11:41 +0200

    [master]: Upload project

---
+++ src/main/java/lv/acodemy/Main.java
@@ -0,0 +1,8 @@
+package lv.acodemy;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Nikita. This is my first project.");
+        System.out.println("Hello World");
+    }
+}
